2a83dcb8514782c6f64abed3a3319bf2771c9256
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-01-02 21:07:46 +0530

    Committing files

---
+++ CodeChef/C++14/DCP/59964832.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define mod 1000000007
+#define mod1 998244353
+#define inf 1e18
+#define nline "\n"
+#define PI 3.141592653589793238462
+#define read(arr, n)            \
+    for (int i = 0; i < n; i++) \
+        cin >> arr[i];
+#define write(arr, n)          \
+    for (int i = 0; i < n; i++) \
+        cout << arr[i] << " " ; \
+    cout << "\n" ; 
+#define loop(i, n) for (int i = 0; i < n; i++)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void init_code()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void debug_code()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("debug.txt", "w", stderr);
+    #endif
+}
+
+signed main(int argc, char **argv){
+    fastio();
+    // init_code();
+    // debug_code();
+
+    ll n, m;
+    cin >> n >> m;
+    map<ll,ll>mp;
+    loop(i, n){
+        ll w;
+        cin >> w;
+        mp[i+1] = w;
+    }
+
+    while(m--){
+        ll c, x;    cin >> c >> x;
+        ll amount = mp[c];
+        while(x--){
+            ll w, cr; cin >> w >> cr;
+            mp[cr] = (mp[cr] + w*amount)%mod;
+        }
+        mp[c] = 0;
+    }
+
+    for(auto x : mp)
+        cout << x.second << endl;
+
+
+
+    return 0;
+
+
+}
+
