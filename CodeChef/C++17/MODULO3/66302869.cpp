7f750a9464c036f7e2702301f209b5c6ec6277e4
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-01-02 21:07:12 +0530

    Committing files

---
+++ CodeChef/C++17/MODULO3/66302869.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define mod 1000000007
+#define nline "\n"
+#define read(arr, n)            \
+    for (int i = 0; i < n; i++) \
+        cin >> arr[i];
+#define write(arr, n)          \
+    for (int i = 0; i < n; i++) \
+        cout << arr[i] << " " ; \
+    cout << "\n" ; 
+#define loop(i, n) for (int i = 0; i < n; i++)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void init_code()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("debug.txt", "w", stderr);
+    #endif
+}
+
+
+signed main(int argc, char **argv){
+    fastio();
+    // init_code();
+
+    int t = 0;
+    cin >> t;
+
+    while (t--){
+        int a, b;
+        cin >> a >> b;
+        if ( a % 3 == 0 || b % 3 == 0)
+            cout << 0 << nline;
+        else{
+            if (a % 3 == b % 3)
+                cout << 1 << nline;
+            else
+                cout << 2 << nline;
+        }
+
+    }
+    return 0;
+
+
+}
+
